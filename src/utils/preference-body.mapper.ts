000eb7361ac8c07a7f5c34d3121ba544c51e70e2
AgustinCarnvale <carnevaleagustinj@gmail.com>
2024-08-18 16:08:49 -0300

    refactor back url into payment body mapper

--- src/utils/preference-body.mapper.ts
+++ src/utils/preference-body.mapper.ts
@@ -36,7 +36,7 @@ export function preferenceBodyMapper(
       },
       back_urls: {
         success:
-          "https://1e59-2803-9800-98c2-6f96-81ff-c6a4-5379-1d8c.ngrok-free.app/api/success", //reenvia a la pagina de gamboost con exito
+          "https://1e59-2803-9800-98c2-6f96-81ff-c6a4-5379-1d8c.ngrok-free.app/api/notification/success", //reenvia a la pagina de gamboost con exito
         //     failure: "http://localhost:8080/failure", //reenvia a la pagina de gamboost con error
         //     pending: "http://localhost:8080/pending", //reenvia a la pagina de gamboost con pendiente
       },
