4f7ca37806590882510095cb6d4adfc564f95a55
AgustinCarnvale <carnevaleagustinj@gmail.com>
2024-09-04 10:44:48 -0300

    fix create payment if there is no available numbers

---
+++ src/modules/payment/application/errors/create-payment.errors.ts
@@ -0,0 +1,6 @@
+export const createPaymentErrors = {
+  notEnoughNumbers: "There are not enough numbers available",
+  failedToCreatePayment: "Failed to create payment",
+  failedToCreatePaymentLink: "Failed to create payment link",
+  failedToUpdatePayment: "Failed to update payment",
+};
